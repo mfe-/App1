79de6c1acfb39d8b420ff6fda814d1046aec7a8c
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-10-24 12:32:28 +0200

    exception not catched

---
+++ App1/HandleException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class AsyncErrorHandler
+{
+    public static void HandleException(Exception exception)
+    {
+        Debug.WriteLine("Exception occurred: " + exception.Message);
+    }
+}
